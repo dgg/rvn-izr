5af3f98f05867ac91559cbb492c79cdb536eb161
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2015-02-23 20:17:27 +0100

    add CreateInAttribute

---
+++ src/Rvn.Izr/CreateInAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Rvn.Izr
+{
+	[AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+	internal sealed class CreateInAttribute : Attribute
+	{
+		public CreateInAttribute(string database)
+		{
+			if (database == null) throw new ArgumentNullException("database", "Databse cannot be null");
+			if (string.IsNullOrWhiteSpace(database)) throw new ArgumentException("Database cannot be empty");
+
+			Database = database;
+		}
+
+		public string Database { get; private set; }
+
+		public override string ToString()
+		{
+			return Database;
+		}
+	}
+}
