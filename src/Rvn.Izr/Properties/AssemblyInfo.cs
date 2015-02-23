9ff60aafac0fb040ec11451c740437b03f5e014c
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2015-02-23 20:12:17 +0100

    empty project with dependency

---
+++ src/Rvn.Izr/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rvn.Izr")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("Using RavenDB My Way")]
+[assembly: AssemblyCompany("Daniel González García")]
+[assembly: AssemblyProduct("rvn-izr")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a085ae9f-88f9-4fef-a355-c42a821e2c38")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
