648fff7f24e009358e610e32d57bb66826ed4a52
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2015-02-23 21:11:59 +0100

    include sample usage

--- src/Rvn.Izr.Tests/IndexesTester.cs
+++ src/Rvn.Izr.Tests/IndexesTester.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.Composition.Hosting;
 using NUnit.Framework;
+using Raven.Client;
+using Raven.Client.Document;
 using Raven.Client.Indexes;
 using Rvn.Izr.Tests.Support;
 
@@ -22,6 +24,22 @@ namespace Rvn.Izr.Tests
 				.Exactly(1).Matches(Is.InstanceOf<Decorated_MyDb>()).And
 				.None.Matches(Is.InstanceOf<NotDecorated>()).And
 				.None.Matches(Is.InstanceOf<Decorated_YourDb>()));
+		}
+
+		[Test, Category("usage"), Ignore]
+		public void IndexesBuilder()
+		{
+			string aDatabase = "a_database";
+
+			IDocumentStore initializedStore = new DocumentStore()
+			{
+				DefaultDatabase = aDatabase
+			};
+
+			ExportProvider export = Indexes.ContainedBesides(typeof(IndexesTester))
+				.For(aDatabase);
+
+			IndexCreation.CreateIndexes(export, initializedStore);
 		}
 	}
 }
